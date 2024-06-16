1b239ad89bafb10a4215fbca001626a90754714b
genie360s <alexgmkwizu@gmail.com>
2024-06-17 01:02:31 +0300

    finished tz_usd spider

--- finscrap/finscrap/spiders/tz_usd_spider.py
+++ finscrap/finscrap/spiders/tz_usd_spider.py
@@ -10,12 +10,19 @@ class TzUsdSpider(scrapy.Spider):
         table = response.css('table')
         table_body = table.css('tbody')
         table_rows = table_body.css('tr')
-        tz_forex_usd_data = table_rows.css('td::text').getall()
+        # tz_forex_usd_datas = table_rows.css('td::text').getall()
+        tz_forex_usd_datas = []
+        for row in table_rows:
+            tz_forex_usd_data = row.css('td::text').getall()
+            tz_forex_usd_datas.append(tz_forex_usd_data)
+        
+        for tz_forex_usd_data in tz_forex_usd_datas:
 
-        yield {
-        'date': tz_forex_usd_data[0].strip() if tz_forex_usd_data[0] else None,
-        'amount': tz_forex_usd_data[1].strip().replace(",", "") if tz_forex_usd_data[1] else None,
-        'high': tz_forex_usd_data[2].strip().replace(",", "") if tz_forex_usd_data[2] else None,
-        'low': tz_forex_usd_data[3].strip().replace(",", "") if tz_forex_usd_data[3] else None,
-        'average': tz_forex_usd_data[4].strip().replace(",", "") if tz_forex_usd_data[4] else None,
-        }
+            yield {
+            
+            'date': tz_forex_usd_data[1].strip() if tz_forex_usd_data[1] else None,
+            'amount': tz_forex_usd_data[2].strip().replace(",", "") if tz_forex_usd_data[2] else None,
+            'high': tz_forex_usd_data[3].strip().replace(",", "") if tz_forex_usd_data[3] else None,
+            'low': tz_forex_usd_data[4].strip().replace(",", "") if tz_forex_usd_data[4] else None,
+            'average': tz_forex_usd_data[5].strip().replace(",", "") if tz_forex_usd_data[5] else None,
+            }
