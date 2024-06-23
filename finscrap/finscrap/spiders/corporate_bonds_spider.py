0312f2910e774d9c10f4687f50f8351c6cdbd2e8
genie360s <alexgmkwizu@gmail.com>
2024-06-23 13:25:01 +0300

    add: government & corporate bonds data spiders

---
+++ finscrap/finscrap/spiders/corporate_bonds_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+
+class CorporateBondsSpider(scrapy.Spider):
+    name = "corporate_bonds"
+
+    start_urls = ['https://dse.co.tz/corporate/bond',]
+
+    def parse(self, response):
+        bond_table = response.css('table.common_table.tableScroll')
+        print(bond_table)
+
+        bond_table_body = bond_table.css('tbody')
+        bond_table_rows = bond_table_body.css('tr')
+
+        bond_datas = []
+        for row in bond_table_rows:
+            bond_data = row.css('td::text').getall()
+            bond_datas.append(bond_data)
+            print(bond_datas)
+
+        def safe_float_conversion(value):
+            try:
+                return float(value.replace("%", "").strip())
+            except ValueError:
+                return None
+
+        for data in bond_datas:
+            # variables are issuer , issued date , coupon rate, issued amount (float), term (int), maturity date
+            #nmb has issues price in USD 
+            yield {
+                'issuer': data[0].strip() if data else None,
+                'issued_date': data[1].strip() if data else None,
+                'coupon_rate': safe_float_conversion(data[2]) if data[2] else None,
+                'issued_amount': float(data[3].replace(",","").replace("USD","").strip()) if data else None,
+                'term_years': int(data[4].replace("Years", "").strip()) if data else None,
+                'maturity_date': data[5].strip() if data else None,
+            }
