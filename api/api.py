ba38a01849adbcb38447c2853f897103a457ef08
genie360s <alexgmkwizu@gmail.com>
2024-07-03 14:06:07 +0300

    create: dse stock prices api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,34 @@
-from . import create_app
 
-api_app = create_app()
+import os
+from flask import Flask, jsonify, request
+import psycopg
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg.connect(
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        dbname = os.getenv("DB_NAME")
+    )
+    return conn
+
+@app.route('/api/v1/dse_stock_prices', methods=['GET'])
+def get_data():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM dse_stock_prices;')
+        rows = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]
+        result = [dict(zip(column_names, row)) for row in rows]
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    api_app.run(debug=True)
+    app.run(debug=True)
