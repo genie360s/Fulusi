9d41d87fe29edf59a81c1ee97b881803fbbb4804
genie360s <alexgmkwizu@gmail.com>
2024-07-09 23:04:35 +0300

    add: latest government bonds api

--- api/api.py
+++ api/api.py
@@ -132,5 +132,39 @@ def get_azania_bank_latest_forex_rates():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+# government bonds latest api
+@app.route('/api/v1/government_bonds/latest_date', methods=['GET'])
+def get_government_bonds_latest_date():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Query to get the latest date
+        cursor.execute('SELECT MAX(DATE(created_at)) FROM government_bonds;')
+        latest_date_row = cursor.fetchone()
+        latest_date = latest_date_row[0]
+
+        if not latest_date:
+            return jsonify({"error": "No data available"}), 404
+
+        # Query to get entries for the latest date
+        query = '''
+            SELECT * FROM government_bonds
+            WHERE DATE(created_at) = %s;
+        '''
+        cursor.execute(query, (latest_date, ))
+        rows = cursor.fetchall()
+
+        if not rows:
+            return jsonify({"error": "No data found for the latest date"}), 404
+
+        column_names = [desc[0] for desc in cursor.description]
+        result = [dict(zip(column_names, row)) for row in rows]
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
