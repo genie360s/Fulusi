9f9b7753760789f3ad877182b5e71dafdc084e3e
genie360s <alexgmkwizu@gmail.com>
2024-07-23 22:08:28 +0300

    add:faid fund latest valuation api

--- api/api.py
+++ api/api.py
@@ -607,5 +607,39 @@ def get_international_commercial_bank_latest_date():
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)})
+    
+#faida fund table latest valuation api
+@app.route('/api/v1/faida_fund/latest_date', methods=['GET'])
+def get_faida_fund_latest_valuation():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Query to get the latest date
+        cursor.execute('SELECT MAX(DATE(created_at)) FROM faida_fund;')
+        latest_date_row = cursor.fetchone()
+        latest_date = latest_date_row[0]
+
+        if not latest_date:
+            return jsonify({"error": "No data available"}), 404
+
+        # Query to get entries for the latest date
+        query = '''
+            SELECT * FROM faida_fund
+            WHERE DATE(created_at) = %s;
+        '''
+        cursor.execute(query, (latest_date, ))
+        rows = cursor.fetchall()
+
+        if not rows:
+            return jsonify({"error": "No data found for the latest date"}), 404
+
+        column_names = [desc[0] for desc in cursor.description]
+        result = [dict(zip(column_names, row)) for row in rows]
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 if __name__ == '__main__':
     app.run(debug=True)
