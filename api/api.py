80257ca0f85d9c2ed46aaa2579011b654e59c31f
genie360s <alexgmkwizu@gmail.com>
2024-07-31 23:03:06 +0300

    update: removed duplicate api's

--- api/api.py
+++ api/api.py
@@ -540,39 +540,7 @@ def get_national_microfinance_bank_latest_date():
     except Exception as e:
         return jsonify({"error": str(e)})
     
-#tanzania commercial bank latest forex rate api
-@app.route('/api/v1/tanzania_commercial_bank/latest_date', methods=['GET'])
-def get_tanzania_commercial_bank_latest_date():
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
 
-        # Query to get the latest date
-        cursor.execute('SELECT MAX(DATE(created_at)) FROM tanzania_commercial_bank;')
-        latest_date_row = cursor.fetchone()
-        latest_date = latest_date_row[0]
-
-        if not latest_date:
-            return jsonify({"error": "No data available"}), 404
-
-        # Query to get entries for the latest date
-        query = '''
-            SELECT * FROM tanzania_commercial_bank
-            WHERE DATE(created_at) = %s;
-        '''
-        cursor.execute(query, (latest_date, ))
-        rows = cursor.fetchall()
-
-        if not rows:
-            return jsonify({"error": "No data found for the latest date"}), 404
-
-        column_names = [desc[0] for desc in cursor.description]
-        result = [dict(zip(column_names, row)) for row in rows]
-        cursor.close()
-        connection.close()
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)})
     
 #international commercial bank latest forex rate api
 @app.route('/api/v1/international_commercial_bank/latest_date', methods=['GET'])
@@ -712,7 +680,7 @@ def get_government_bonds():
 
 #corporate bonds latest entry 
 @app.route('/api/v1/corporate_bonds/latest_entry', methods=['GET'])
-def get_uttamis_fund_latest_valuation():
+def get_corporate_bonds_latest_valuation():
     try:
         connection = get_db_connection()
         cursor = connection.cursor()
